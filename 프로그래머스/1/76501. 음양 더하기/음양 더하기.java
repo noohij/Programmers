def2011ff9057f814104098f73d6a78a55ab832c
Jihoon PARK <105366274+noohij@users.noreply.github.com>
2023-11-17 18:24:56 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,17 +1,20 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
+        int[] intArr = new int[absolutes.length];
         
         for(int i = 0; i < absolutes.length; i++){
-            if(signs[i] == false){
-                absolutes[i] = absolutes[i] * -1;
+            if(signs[i]){
+                intArr[i] = absolutes[i];
+            } else{
+                intArr[i] = -absolutes[i];
             }
         }
         
-        for(int j = 0; j < absolutes.length; j++){
-            answer += absolutes[j];
+        for(int j = 0; j < intArr.length; j++){
+            answer += intArr[j];
         }
-        
+    
         return answer;
     }
 }
